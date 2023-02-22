055a9182cbfc7fc12261fbd2aca54d069f225d87
AP696 <addyphilips66@gmail.com>
2023-02-22 11:10:31 +0000

    1

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
 })
+
+app.get('/musicians', async (req, res) => {
+    try {
+        const musicians = Musician.findAll();
+        res.json(musicians);
+    } catch (err) {
+        console.log(err)
+        res.status(500)
+    }
+
+})

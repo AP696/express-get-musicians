94c2c1097011ce00c08c6a36e69111282dde7170
Addison Philips <addyphilips66@gmail.com>
2023-02-28 15:54:05 +0000

    12

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const port = 3000;
 
 //TODO
 
+app.use(express.json());
+
 app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
-})
+  sequelize.sync();
+  console.log(`Listening on port ${port}`);
+});
 
 app.get("/musicians", async (req, res) => {
   try {
@@ -29,5 +31,58 @@ app.get("/musicians/:id", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send("Cannot find the musician you are looking for");
+  }
+});
+
+app.post("/musicians", async (req, res) => {
+  try {
+    const { name, instrument } = req.body;
+    const musician = await Musician.create({
+      name,
+      instrument,
+    });
+    res.status(201).json(musician);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while creating the musician");
+  }
+});
+
+app.put("/musicians/:id", async (req, res) => {
+  try {
+    const { name, instrument } = req.body;
+    const updatedMusician = await Musician.update(
+      {
+        name,
+        instrument,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    if (updatedMusician[0]) {
+      res.status(200).send("Musician updated successfully");
+    } else {
+      res.status(404).send("Musician not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while retrieving musicians");
+  }
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+  try {
+    const deletedMusician = await Musician.destroy({
+      where: { id: req.params.id },
+    });
+    if (deletedMusician) {
+      res.status(200).send("Musician deleted successfully");
+    } else {
+      res.status(404).send("Musician not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while deleting musician");
   }
 });
